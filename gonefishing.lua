aae39dfc6984d0765c5ddd410955c02cc6043265
Christian Garbs <mitch@cgarbs.de>
2011-12-25 19:28:33 +0100

    update indentation

--- gonefishing.lua
+++ gonefishing.lua
@@ -20,61 +20,62 @@ GONEFISHING_rodlist = {
 SLASH_GoneFishing1 = "/gonefishing";
 
 -- register command handler
-SlashCmdList["GoneFishing"] = function()
+SlashCmdList["GoneFishing"] =
+   function()
 
-				 -- get slot IDs
-				 local invSlotRightHand, textureName = GetInventorySlotInfo("MainHandSlot");
-				 local invSlotLeftHand,  textureName = GetInventorySlotInfo("SecondaryHandSlot");
-
-				 if GONEFISHING_isFishing == nil or GONEFISHING_isFishing == 0 then
-
-				    -- remember items in hand
-				    rightHandItemId = GetInventoryItemID("player", invSlotRightHand);
-				    leftHandItemId  = GetInventoryItemID("player", invSlotLeftHand);
-
-				    -- iterate over pole list
-				    local i = 0;
-				    local continue = 1;
-
-				    while continue == 1 do
-
-				       -- get item name from list
-				       i = i + 1;
-				       local itemname = GONEFISHING_rodlist[i];
-
-				       if itemname == nil then
-
-					  -- end of list
-					  continue = 0;
-
-				       else
-
-					  -- equip fishing pole
-					  EquipItemByName(itemname);
-
-					  -- did it work?
-					  local newRightHandItemId = GetInventoryItemID("player", invSlotRightHand);
-					  if newRightHandItemId ~= rightHandItemId then
-					     continue = 0;
-					  end
-
-				       end
-
-				    end
-
-				    -- change state to 'fishing'
-				    GONEFISHING_isFishing = 1;
-
-				 else
-				    
-				    -- restore saved equipment
-				    EquipItemByName(rightHandItemId, invSlotRightHand);
-				    EquipItemByName(leftHandItemId,  invSlotLeftHand);
-
-				    -- change state to 'not fishing'
-				    GONEFISHING_isFishing = 0;
-
-				 end
-
-			      end
+      -- get slot IDs
+      local invSlotRightHand, textureName = GetInventorySlotInfo("MainHandSlot");
+      local invSlotLeftHand,  textureName = GetInventorySlotInfo("SecondaryHandSlot");
+      
+      if GONEFISHING_isFishing == nil or GONEFISHING_isFishing == 0 then
+	 
+	 -- remember items in hand
+	 rightHandItemId = GetInventoryItemID("player", invSlotRightHand);
+	 leftHandItemId  = GetInventoryItemID("player", invSlotLeftHand);
+	 
+	 -- iterate over pole list
+	 local i = 0;
+	 local continue = 1;
+	 
+	 while continue == 1 do
+	    
+	    -- get item name from list
+	    i = i + 1;
+	    local itemname = GONEFISHING_rodlist[i];
+	    
+	    if itemname == nil then
+	       
+	       -- end of list
+	       continue = 0;
+	       
+	    else
+	       
+	       -- equip fishing pole
+	       EquipItemByName(itemname);
+	       
+	       -- did it work?
+	       local newRightHandItemId = GetInventoryItemID("player", invSlotRightHand);
+	       if newRightHandItemId ~= rightHandItemId then
+		  continue = 0;
+	       end
+	       
+	    end
+	    
+	 end
+	 
+	 -- change state to 'fishing'
+	 GONEFISHING_isFishing = 1;
+	 
+      else
+	 
+	 -- restore saved equipment
+	 EquipItemByName(rightHandItemId, invSlotRightHand);
+	 EquipItemByName(leftHandItemId,  invSlotLeftHand);
+	 
+	 -- change state to 'not fishing'
+	 GONEFISHING_isFishing = 0;
+	 
+      end
+      
+   end
 
