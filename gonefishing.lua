8d627c7ad207bca096e96c25c2f6327e11ad336d
Christian Garbs <mitch@cgarbs.de>
2011-12-25 19:27:38 +0100

    add support for different fishing poles
    (fishing pole, not fishing rod?)

--- gonefishing.lua
+++ gonefishing.lua
@@ -1,6 +1,21 @@
 -- set state to 'not fishing'
 GONEFISHING_isFishing = 0;
 
+-- list of fishing poles, best pole first
+GONEFISHING_rodlist = {
+   "Knöcherne Angelrute",
+   "Bone Fishing Pole",
+
+   "Starke Angelrute",
+   "Strong Fishing Pole",
+
+   "Angelrute",
+   "Fishing Pole",
+
+   "Einfache Angelrute",
+   "Basic Fishing Pole"
+};
+
 -- set slash command
 SLASH_GoneFishing1 = "/gonefishing";
 
@@ -17,8 +32,35 @@ SlashCmdList["GoneFishing"] = function()
 				    rightHandItemId = GetInventoryItemID("player", invSlotRightHand);
 				    leftHandItemId  = GetInventoryItemID("player", invSlotLeftHand);
 
-				    -- equip fishing rod
-				    EquipItemByName("Starke Angelrute");
+				    -- iterate over pole list
+				    local i = 0;
+				    local continue = 1;
+
+				    while continue == 1 do
+
+				       -- get item name from list
+				       i = i + 1;
+				       local itemname = GONEFISHING_rodlist[i];
+
+				       if itemname == nil then
+
+					  -- end of list
+					  continue = 0;
+
+				       else
+
+					  -- equip fishing pole
+					  EquipItemByName(itemname);
+
+					  -- did it work?
+					  local newRightHandItemId = GetInventoryItemID("player", invSlotRightHand);
+					  if newRightHandItemId ~= rightHandItemId then
+					     continue = 0;
+					  end
+
+				       end
+
+				    end
 
 				    -- change state to 'fishing'
 				    GONEFISHING_isFishing = 1;
